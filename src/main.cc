286e841f7c0663624186280abc12fce1b1759f5e
zhuyk6 <zhuyk6@foxmail.com>
2024-08-30 21:00:48 +0800

    Fix code.
    - Remove `format`.
    - Remove setup GCC in CI.

--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,15 @@
 #include "foo.h"
-#include <format>
+// #include <format>
 #include <iostream>
 
 int main() {
-    Foo foo;
-    int x = 2;
-    int y = 3;
-    auto ret = std::format("Hello, {} + {} = {}", x, y, foo.add(x, y));
-    std::cout << ret << std::endl;
+  Foo foo;
+  int x = 2;
+  int y = 3;
+  // auto ret = std::format("Hello, {} + {} = {}", x, y, foo.add(x, y));
+  // std::cout << ret << std::endl;
+  std::cout << "Hello, " << x << " + " << y << " = " << foo.add(x, y)
+            << std::endl;
 
-    return 0;
+  return 0;
 }
